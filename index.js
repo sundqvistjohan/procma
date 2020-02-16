f695f46e89abc09ba8744e001acced298fc5dacb
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-16 13:31:44 +0100

    Adds queries to index

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const db = require('./queries')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
-
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+app.get('/articles', db.getArticles)
+app.get('/articles/:id', db.getArticleById)
+app.post('/articles', db.createArticle)
